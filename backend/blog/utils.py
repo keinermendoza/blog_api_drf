26a96d8230b1f888bc62507d7cac185a6493ec54
keiner <keinermendoza@gmail.com>
2023-10-04 21:16:22 -0400

    base functionality of send html emails ready

--- backend/blog/utils.py
+++ backend/blog/utils.py
@@ -1,25 +1,55 @@
-from django.core import mail
+from django.core.mail import EmailMessage
 from django.template.loader import render_to_string
 from django.utils.html import strip_tags
 from django.utils import timezone
+from rest_framework_simplejwt.tokens import RefreshToken
 
-class MailSender:
+class SendMailTo:
     
     SITE = 'http://localhost:8000/admin/'
     FROM_EMAIL = 'From keiner@example.com'
-            
-    @classmethod
-    def wellcome(cls, user_data):
+    WEBSITE_NAME = "Keiner's blog"
+
+    def __init__(self, user_instance):
+        self.user = user_instance
+        self.subject = None
+        self.html_message = None
+        self.plain_message = None
+
+    def wellcome_email(self):
+        """sends a wellcome message to the current user"""
         context = {
-            'username': user_data['username'],
+            'username': self.user.username,
+            'website': self.WEBSITE_NAME,
             'date' : timezone.now(),
-            'url': SITE,
+            'url': self.SITE,
         }
 
-        subject = "Wellcome to the Keiner's Blog"
-        html_message = render_to_string('mail/wellcome.html', context)
-        plain_message = strip_tags(html_message)
-        to = 'keiner@example.com'
+        self.subject = "Wellcome to the Keiner's Blog"
+        self.html_message = render_to_string('mail/wellcome.html', context)
+        self.plain_message = strip_tags(self.html_message)
+        self.send()
 
-        # to = user_data['to']
-        mail.send_mail(subject, plain_message, cls.FROM_EMAIL, [to], html_message)
+    def authentication_email(self):
+        """sends an email with the url-token authentication for new users"""
+
+        token = RefreshToken.for_user(self.user)
+
+        context = {
+            'username': self.user.username,
+            'website': self.WEBSITE_NAME,
+            'date' : timezone.now(),
+            'url': self.SITE + str(token),
+        }
+
+        self.subject = "Validate your email on Keiner's Blog"
+        self.html_message = render_to_string('mail/authenticate.html', context)
+        self.plain_message = strip_tags(self.html_message)
+        self.send()
+
+    def send(self):
+        """sends an email using the current values of the Mail class and instance"""
+        message = EmailMessage(self.subject, self.html_message, self.FROM_EMAIL, [self.user.email])
+        message.content_subtype = 'html' # this is required because there is no plain text email message
+        message.send()
+
--- backend/blog/views.py
+++ backend/blog/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework import generics, permissions
-from .models import Post
+from .models import Post, User
 from .serializers import PostSerializer
 from .permissions import IsAuthorOrReadOnly
-from .utils import MailSender
+from .utils import SendMailTo
 
 class PostList(generics.ListCreateAPIView):
     queryset = Post.objects.all()
@@ -19,12 +19,17 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = PostSerializer
     permission_classes = (IsAuthorOrReadOnly,)
 
-# curl -X POST http://localhost:8000/api/v1/send_email -H "Content-Type: application/json"
-#    -d '{"username": "boby", "mail": "holamundo@gmail.com"}'
 
+# curl -X POST http://localhost:8000/api/v1/send_email -d "email=someemail@gmail.com"
 @csrf_exempt
 def send_email(request):
-    username = request.POST.get('username')
-    mail = request.POST.get('mail')
-    MailSender.wellcome(user_data={'username':username, 'mail':mail})
-    return HttpResponse(status=204)
+    
+    email = request.POST.get('email')
+    if email:
+        if user := User.objects.get(email=email):
+            print(user)
+            SendMailTo(user).authentication_email()
+            SendMailTo(user).wellcome_email()
+            
+            return HttpResponse(status=204)
+    return HttpResponse(status=400)
